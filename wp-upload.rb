825b5fc18af7b982aeb6e4e3562c490276893a97
Jennifer Mack <jenn@jennifermack.net>
2016-05-20 04:42:32 -0700

    Initial commit

---
+++ wp-upload.rb
@@ -0,0 +1,56 @@
+#! /usr/bin/env ruby
+
+require 'xmlrpc/client'
+require 'xmlrpc/Base64'
+require 'json'
+
+begin
+  pwfile = File.expand_path("~/.upload-wp-password.json")
+  if !File.readable?(pwfile) then
+    pwfile = File.expand_path("~/.ulysses-wp-password.json")
+  end
+  pwinfo = JSON.parse(File.open(pwfile).read)
+rescue => err
+  puts "Password error: #{err}"
+  exit
+end
+
+begin
+  wordpress = XMLRPC::Client.new_from_hash({
+    "host"     => pwinfo["host"],
+    "user"     => pwinfo["user"],
+    "password" => pwinfo["password"],
+    "use_ssl"  => pwinfo["use_ssl"],
+    "path"     => "/xmlrpc.php"
+  })
+rescue => err
+  puts "Build XMLRPC error: #{err}"
+  exit
+end
+
+ARGV.each do |f|
+  begin
+  data = {
+    :bits => XMLRPC::Base64.new(File.open(f).read),
+    :name => File.basename(f),
+    :type => "image/#{File.extname(f)}"
+  }
+  rescue => err
+    puts "Data encode error: #{err}"
+    exit
+  end
+
+  begin
+    result = wordpress.call(
+      'wp.uploadFile',
+      0,
+      wordpress.user,
+      wordpress.password, data
+    )
+    puts "Uploaded: #{File.basename(result["url"])}"
+  rescue => err
+    puts "Upload error: #{err}"
+    exit
+  end
+end
+
